f43351188c46c7cf0f4738432a86b8bb1eb3a530
Paweł Żabczyński <p_k_zab@wp.pl>
2025-03-02 18:49:11 +0100

    drugi podpunkt

--- lab1/zad1.py
+++ lab1/zad1.py
@@ -2,11 +2,26 @@ import math
 import datetime
 
 
-
+wave_length = {
+    "fizyczna":23,
+    "intelektualna":28,
+    "emocjonalna":33}
 
 
 def get_wave(t,cycle):
     return math.sin((2*math.pi/cycle)*t)
+
+def wave_message(type,t):
+    value = get_wave(t,wave_length[type])
+    print(f"Twoja fala {type} wynosi {value}")
+    if value <= -0.5:
+        print("Wygląda na to że masz pod tym względem słaby dzień, dasz radę c:")
+        if value > get_wave(t+1,wave_length[type]):
+            print("I patrz pozytywnie! Jutro będzie lepiej :D")
+    if value >= 0.5:
+        print("Wygląda na to że masz dobry dzień pod tym względem :]")
+    
+
 
 print("Enter your name:")
 name = input()
@@ -19,7 +34,9 @@ day = int(input())
 
 t=(datetime.datetime.now().date() - datetime.date(year,month,day)).days
 
-print(f"Hello {name} you have been alive for {t} days!")
-print(f"Fizyczna fala {get_wave(t,23)}")
-print(f"Intelektualna fala {get_wave(t,28)}")
-print(f"Emocjonalna fala {get_wave(t,33)}")
+
+print(f"Cześć {name}! Żyjesz już {t} dni!")
+
+wave_message("fizyczna",t)
+wave_message("intelektualna",t)
+wave_message("emocjonalna",t)
